5c7700716699975ce52fbffb0c5651a2427c2f11
Victor <victorchagasms@gmail.com>
2022-02-13 01:02:11 -0300

    update: modificado a url base de index para o login add: path para realizar agendamentos

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.login, name = 'login'),
     path('usuario/cadastro', views.cadastro, name='cadastro'),
-    path('usuario/login', views.login, name='login'),
     path('home', views.home, name ='home'),
     path('logout', views.logout_user, name = 'logout'),
-    path('agendamento', views.agendamento, name ='agendamento'),
-    path('listagem', views.listagem_agendamentos, name ='listagem')
+    path('buscar_agendamentos', views.buscar_agendamentos, name ='buscar_agendamentos'),
+    path('listagem', views.listagem_agendamentos, name ='listagem'),
+    path('realizar_agendamento/<int:id_agendamento>/', views.realizar_agendamento, name='realizar_agendamento'),
 
 ]
